7b6ec7c923aa64bf40b97334339046a3a482b509
xpertdreamer <yungnomen@gmail.com>
2026-08-23 22:18:03 +0300

    Macroces to alloc and free gates

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include <stdint.h>
 #include <stdbool.h>
-
-
+#include <stdlib.h>
+#include <stdio.h>
 
 typedef enum {
    GATE_AND = 0,
@@ -17,6 +17,34 @@ typedef struct {
     _Bool output;
     _Bool *inputs;
 } Gate;
+
+#define ALLOC_GATE(ptr, type, inputs) do {                      \
+    ptr = (Gate*)malloc(sizeof(Gate));                          \
+    if ((ptr) == NULL) {                                        \
+        fprintf(stderr, "Gate allocation failed\n");            \
+        exit(EXIT_FAILURE);                                     \
+    }                                                           \
+    (ptr)->inputs_count = (inputs);                             \
+    (ptr)->inputs = (_Bool*)malloc((inputs) * sizeof(_Bool));           \
+    if ((ptr)->inputs == NULL) {                                        \
+        fprintf(stderr, "Inputs allocation failed\n");                  \
+        free(ptr);                                                      \
+        exit(EXIT_FAILURE);                                             \
+    }                                                                   \
+} while (0)
+
+#define FREE_GATE(ptr) do                       \
+        if ((ptr) != NULL) {                    \
+            if ((ptr)->inputs != NULL) {        \
+                free((ptr)->inputs);            \
+                (ptr)->inputs = NULL;           \
+            }                                   \
+            free(ptr);                          \
+            (ptr) = NULL;                       \
+        }                                       \
+    free(ptr);                                  \
+    (ptr) = NULL;                               \
+} while(0)
 
 int main(void) {
 
