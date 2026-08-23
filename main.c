40fa75c11eee973194fd462b97d4e31a652321bd
xpertdreamer <yungnomen@gmail.com>
2026-08-23 22:30:04 +0300

    Raylib drawing loop

--- main.c
+++ main.c
@@ -1,7 +1,10 @@
 #include <stdint.h>
 #include <stdbool.h>
 #include <stdlib.h>
-#include <stdio.h>
+#include <raylib.h>
+
+#define RAYGUI_IMPLEMENTATION
+#include "thirdparty/raygui.h"
 
 typedef enum {
    GATE_AND = 0,
@@ -18,14 +21,14 @@ typedef struct {
     _Bool *inputs;
 } Gate;
 
-#define ALLOC_GATE(ptr, type, inputs) do {                      \
+#define ALLOC_GATE(ptr, type, input_c) do {                     \
     ptr = (Gate*)malloc(sizeof(Gate));                          \
     if ((ptr) == NULL) {                                        \
         fprintf(stderr, "Gate allocation failed\n");            \
         exit(EXIT_FAILURE);                                     \
     }                                                           \
-    (ptr)->inputs_count = (inputs);                             \
-    (ptr)->inputs = (_Bool*)malloc((inputs) * sizeof(_Bool));           \
+    (ptr)->inputs_count = (input_c);                                    \
+    (ptr)->inputs = (_Bool*)malloc((input_c) * sizeof(_Bool));          \
     if ((ptr)->inputs == NULL) {                                        \
         fprintf(stderr, "Inputs allocation failed\n");                  \
         free(ptr);                                                      \
@@ -33,7 +36,7 @@ typedef struct {
     }                                                                   \
 } while (0)
 
-#define FREE_GATE(ptr) do                       \
+#define FREE_GATE(ptr) do {                     \
         if ((ptr) != NULL) {                    \
             if ((ptr)->inputs != NULL) {        \
                 free((ptr)->inputs);            \
@@ -47,6 +50,12 @@ typedef struct {
 } while(0)
 
 int main(void) {
+    InitWindow(400, 400, "fasm-hello");
+    SetTargetFPS(24);
 
-    return 0;
+    while (!WindowShouldClose()) {
+        BeginDrawing();
+        ClearBackground(GetColor(GuiGetStyle(DEFAULT, BACKGROUND_COLOR)));
+        EndDrawing();
+    }
 }
